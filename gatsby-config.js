69956fc22a7d36b0fb0a42aea35447836b67eb69
aarogacki <alainarogacki@gmail.com>
2019-06-16 20:42:14 -0400

    setting up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,40 @@
 module.exports = {
-  siteMetadata: {
-    navbarLinks: [
-      {to: "/makeup", name: "Makeup"},
-      {to: "/lifestyle", name: "Lifestyle"},
-      {to: "/blog", name: "blog"},
-    ],
-    title: "TYRA",
-    description: "Tyra is a fast, feminine, and chic Gatsby.js theme.",
-    siteUrl: "https://tyra-starter.netlify.com",
-    homepageHeader: "Welcome to Your New Blog",
-    homepageAbout: "Tyra is a modern, sleek and feminine Gatsby.js theme. Easily create a beautiful and fast blog and draw attention to your stellar content.",
-    mailChimpUrl: "https://mailchimp.com",
-    mailChimpToken: "MAILCHIMP TOKEN HERE",
-    youtube: "", // YOUR YOUTUBE PROFILE HERE
-    github: "", // YOUR GITHUB PROFILE HERE 
-    pinterest: "", // YOUR PINTEREST PROFILE HERE
-    facebook: "", // YOUR FACEBOOK PROFILE HERE
-    twitter: "", // YOUR TWITTER PROFILE HERE
-  },
-  plugins: [
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-plugin-feed',
-      options: {
-        query: `
+    siteMetadata: {
+        navbarLinks: [{
+                to: "/patterns",
+                name: "Patterns"
+            },
+            {
+                to: "/lifestyle",
+                name: "Lifestyle"
+            },
+            {
+                to: "/blog",
+                name: "Blog"
+            },
+        ],
+        title: "CrochetBae",
+        description: "Crochetcrochetcrochet",
+        siteUrl: "https://crochetbae.com",
+        homepageHeader: "Welcome to Your New Blog",
+        homepageAbout: "Tyra is a modern, sleek and feminine Gatsby.js theme. Easily create a beautiful and fast blog and draw attention to your stellar content.",
+        mailChimpUrl: "https://mailchimp.com",
+        mailChimpToken: "MAILCHIMP TOKEN HERE",
+        youtube: "", // YOUR YOUTUBE PROFILE HERE
+        github: "", // YOUR GITHUB PROFILE HERE 
+        pinterest: "", // YOUR PINTEREST PROFILE HERE
+        facebook: "", // YOUR FACEBOOK PROFILE HERE
+        twitter: "", // YOUR TWITTER PROFILE HERE
+    },
+    plugins: [
+        'gatsby-plugin-sitemap',
+        'gatsby-plugin-react-helmet',
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+        {
+            resolve: 'gatsby-plugin-feed',
+            options: {
+                query: `
         {
           site {
             siteMetadata {
@@ -38,20 +46,26 @@ module.exports = {
           }
         }
       `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                })
-              })
-            },
-            query: `
+                feeds: [{
+                    serialize: ({
+                        query: {
+                            site,
+                            allMarkdownRemark
+                        }
+                    }) => {
+                        return allMarkdownRemark.edges.map(edge => {
+                            return Object.assign({}, edge.node.frontmatter, {
+                                description: edge.node.excerpt,
+                                date: edge.node.frontmatter.date,
+                                url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+                                guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+                                custom_elements: [{
+                                    "content:encoded": edge.node.html
+                                }],
+                            })
+                        })
+                    },
+                    query: `
             {
               allMarkdownRemark(
                 limit: 1000,
@@ -72,51 +86,50 @@ module.exports = {
               }
             }
           `,
-            output: "/rss.xml",
-            title: "Gatsby RSS Feed",
-          },
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/content`,
-        name: 'content',
-      },
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          'gatsby-remark-copy-linked-files',
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 1400,
+                    output: "/rss.xml",
+                    title: "Gatsby RSS Feed",
+                }, ],
             },
-          },
-        ],
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-web-font-loader',
-      options: {
-        google: {
-          families: ['Karla', 'Playfair Display', 'Lora']
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/content`,
+                name: 'content',
+            },
+        },
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [
+                    'gatsby-remark-copy-linked-files',
+                    {
+                        resolve: 'gatsby-remark-images',
+                        options: {
+                            maxWidth: 1400,
+                        },
+                    },
+                ],
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-web-font-loader',
+            options: {
+                google: {
+                    families: ['Karla', 'Playfair Display', 'Lora']
+                }
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: "",
+                head: false,
+                anonymize: true,
+                respectDNT: true,
+                exclude: ['/success'],
+                cookieDomain: "tyra-starter.netlify.com",
+            }
         }
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: "",
-        head: false,
-        anonymize: true,
-        respectDNT: true,
-        exclude: ['/success'],
-        cookieDomain: "tyra-starter.netlify.com",
-      }
-    }
-  ]
+    ]
 }
